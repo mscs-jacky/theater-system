170e48b20e9bf203f4c6a7786e9479a219f29814
Jacky Li <100312817+mscs-jacky@users.noreply.github.com>
2022-03-14 17:30:20 -0700

    Create Attendant.java

---
+++ TheaterSystem/src/TheaterSystem/Models/Attendant.java
@@ -0,0 +1,24 @@
+package TheaterSystem.Models;
+
+import java.util.UUID;
+
+public class Attendant extends Employee {
+    public Attendant(String firstName, String lastName, double payRate) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmployeeId(String.valueOf(UUID.randomUUID()));
+        setEmployeePassword(String.valueOf(UUID.randomUUID()));
+        setPayRate(payRate);
+    }
+
+    @Override
+    public void getEmployeeDetails() {
+        System.out.println("Name:          " + getName());
+        System.out.println("Employee Type: Attendant");
+        System.out.println("Employee ID:   " + getEmployeeId());
+        System.out.println("Pay Rate:      " + getPayRate());
+        System.out.println("Hours Worked:  " + getAccumulatedWorkHours());
+        System.out.println("Total Pay:     " + getPayRate()*getAccumulatedWorkHours());
+
+    }
+}
