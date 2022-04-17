f0593370a00e703420e6a7e40d89265bd0b35cef
Jacky Li <100312817+mscs-jacky@users.noreply.github.com>
2022-04-17 16:34:43 -0700

    update code logic of getDiscount method

--- TheaterSystem/src/TheaterSystem/Controllers/DiscountFactory.java
+++ TheaterSystem/src/TheaterSystem/Controllers/DiscountFactory.java
@@ -1,15 +1,12 @@
 package TheaterSystem.Controllers;
 
-import TheaterSystem.Models.Attendant;
-import TheaterSystem.Models.Employee;
 import TheaterSystem.Models.Discount;
-import java.util.Scanner;
 
 public class DiscountFactory {
     private Discount[] discountList;
 
-    public void getDiscount(string name){
-        for discount in discountList{
+    public String getDiscount(String name){
+        for (Discount discount : discountList) {
             if(discount.getName() == name){
                 return discount.getName();
             }
