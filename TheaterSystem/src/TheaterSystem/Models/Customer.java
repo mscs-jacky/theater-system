2d7d182e13a74e40d3616b51454b73a9f458c7c6
Jakub Koziol <jakubkoziol99@gmail.com>
2022-04-21 22:11:52 -0700

    observer pattern

--- TheaterSystem/src/TheaterSystem/Models/Customer.java
+++ TheaterSystem/src/TheaterSystem/Models/Customer.java
@@ -1,8 +1,18 @@
 package TheaterSystem.Models;
 
+import java.time.LocalDateTime;
+
 public class Customer extends Person{
+
+    private LocalDateTime showtime;
+
+
     public Customer(String firstName, String lastName) {
         setFirstName(firstName);
         setLastName(lastName);
+    }
+
+    public void Update(ShowTime showTime){
+        this.showtime = showTime.getStartTime();
     }
 }
--- TheaterSystem/src/TheaterSystem/Models/ShowTime.java
+++ TheaterSystem/src/TheaterSystem/Models/ShowTime.java
@@ -1,11 +1,15 @@
 package TheaterSystem.Models;
 import java.time.LocalDateTime; // import the LocalDateTime class
+import java.util.ArrayList;
 
 public class ShowTime {
 
     private LocalDateTime startTime;
 
     private Theater theater;
+
+    private ArrayList<Customer> Attendees;
+
 
     public ShowTime(LocalDateTime startTime, Theater theater){
         this.startTime = startTime;
@@ -18,7 +22,23 @@ public class ShowTime {
 
     public void setStartTime(LocalDateTime startTime){
         this.startTime = startTime;
+        this.notifyCustomer();
     }
+
+    public void attach(Customer customer){
+        Attendees.add(customer);
+    }
+
+    public void detach(Customer customer){
+        Attendees.remove(customer);
+    }
+
+    public void notifyCustomer(){
+        for(Customer Attendee : Attendees){
+            Attendee.Update(this);
+        }
+    }
+
 
     public Theater getTheater() {
         return theater;
